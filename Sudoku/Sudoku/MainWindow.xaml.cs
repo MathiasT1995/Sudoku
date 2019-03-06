1e844554e958ce80fcc4ccd7fc594139c7397173
Mathias Haahr Taankvist <Mathias.taankvist@gmail.com>
2019-03-06 14:43:48 +0100

    Noget virker, noget gør ikke.  kommer vi i mål? Følg med i næste afsnit

--- Sudoku/Sudoku/MainWindow.xaml.cs
+++ Sudoku/Sudoku/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using SudokuLib;
+using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,12 +22,69 @@ namespace Sudoku
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
+    public partial class MainWindow : Window {
+        Button selectedButton = null;
+        
+        public MainWindow(){
+            int[,] sudokuArray = loadSudoku("C:\\4Semester\\CSharp\\Sudoku\\Sudoku\\Sudoku\\top1465.txt");
+            string sudokuString = convertArrayToString(sudokuArray);
+            var s = SudokuFactory.CreateSudoku(sudokuString);
+
+            PrintToConsole(sudokuArray);
             InitializeComponent();
             insertButtons();
+            initializeSudoku(sudokuArray);
+        }
+
+        public void buttonClicked(object sender, RoutedEventArgs e) {
+            Button srcButton = e.Source as Button;
+            selectedButton = srcButton;
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e) {
+            if(selectedButton != null) {
+                if (!Char.IsDigit((char)KeyInterop.VirtualKeyFromKey(e.Key)) & e.Key != Key.Back | e.Key == Key.Space) {
+                    e.Handled = true;
+                    MessageBox.Show("I only accept numbers, sorry. :(", "Error");
+                } else {
+                    selectedButton.Content = e.Key.ToString()[1];
+                }
+            }
+        }
+
+        public void CanIPressThisButton(int input) {
+            int x = selectedButton.Name[3];
+            int y = selectedButton.Name[4];
+        }
+
+        public void PrintToConsole(int[,] input) {
+            for (int i = 0; i < 9; i++) {
+                for (int j = 0; j < 9; j++) {
+                    Console.Write(input[i, j] + " ");
+                }
+                Console.Write("\n");
+            }
+        }
+            public string convertArrayToString(int[,] input) {
+            string res = "";
+            for(int x = 0; x < 9; x++) {
+                for(int y = 0; y < 9; y++) {
+                    res += input[x, y];
+                }
+            }
+            return res;
+        }
+
+        public void initializeSudoku(int[,] input) {
+            for (int x = 0; x < 9; x++) {
+                for (int y = 0; y < 9; y++) {
+                    string tempBtnName = "btn" + x.ToString() + y.ToString();
+                    Button tempBtn = (Button)FindName(tempBtnName);
+                    if(input[y,x] != 0) {
+                    tempBtn.Content = input[y,x];
+                    }
+                }
+            }
         }
 
         public void insertButtons() {
@@ -32,16 +92,42 @@ namespace Sudoku
                 for (int y = 0; y < 9; y++) {
                     System.Windows.Controls.Button btn = new Button();
                     btn.Name = "btn" + x.ToString() + y.ToString();
-                    btn.Content = "" + x.ToString() + y.ToString();
                     Grid.SetColumn(btn, x);
                     Grid.SetRow(btn, y);
-                    
+                    btn.Click += buttonClicked;
                     grid.Children.Add(btn);
-
-
+                    grid.RegisterName(btn.Name, btn);
                 }
+            }
+        }
+
+        public void loadWindow(object sender, RoutedEventArgs e) {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true) {
+                loadSudoku(openFileDialog.FileName);
+            }
+        }
+
+        public int[,] loadSudoku(string path) {
+            int[,] SudokuArray = new int[9, 9];
+               string[] lines = File.ReadAllLines(path);
+               int index = new Random().Next(0, lines.Length-1);
+
+               //Read string and put it in a two dimensional array
+               int x = 0;
+               for (int i = 0; i < 9; i++) {
+                   for (int j = 0; j < 9; j++) {
+                       if (lines[index][x].Equals('.')) {
+                           SudokuArray[i, j] = 0;
+                       } else {
+                           SudokuArray[i, j] = int.Parse(lines[index][x] + "");
+                       }
+                       x++;
+                   }
+               }
+        return SudokuArray;
             }
         }
     }
     
-}
+
